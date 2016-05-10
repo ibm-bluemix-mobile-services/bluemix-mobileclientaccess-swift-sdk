8c9a7f207ae3cf0331c675f1c30b03c42644410d
Oded Betzalel <ODEDB@il.ibm.com>
2016-05-10 10:05:26 +0300

    Update Kitura dependency to support 05-o3 snapshot

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "MobileClientAccessSDK",
     dependencies:[
 		.Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 0),
-		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor:11),
+		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor:13),
 		.Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-logger-swift.git", majorVersion: 0, minor: 1)
 ]
 )
