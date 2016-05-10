1d2c66677294b2640f8f35e2a286e8163c31d4bf
Oded Betzalel <ODEDB@il.ibm.com>
2016-05-10 10:27:40 +0300

    update swift-cfenv dependency

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "MobileClientAccessSDK",
     dependencies:[
-		.Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 0),
+		.Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1),
 //		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor:13),
 		.Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-logger-swift.git", majorVersion: 0, minor: 2)
 ]
